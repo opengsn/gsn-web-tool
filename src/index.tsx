5bd6cd5cb0bdfaf162dbe3443ba97cbbfb5959af
qq7 <qq7dev@gmail.com>
2022-06-24 15:09:27 +0300

    move to using injected connector, provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,70 @@
 import ReactDOM from "react-dom/client";
-import {WagmiConfig, createClient, chain} from 'wagmi'
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
-import { providers } from "ethers";
+import { WagmiConfig, configureChains, defaultChains, createClient, chain } from "wagmi";
+// import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { MetaMaskInpageProvider } from "@metamask/providers";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { providers, getDefaultProvider, ethers } from "ethers";
+import { publicProvider } from 'wagmi/providers/public'
 import {
   BrowserRouter,
   Routes,
-  Route
+  Route,
 } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 import App from "./routes/App";
 import store from "./store";
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 
 import { ErrorBoundary } from "./components/ErrorBoundary";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
 
-const ethProvider = new providers.JsonRpcProvider(process.env.RPC_URL, providers.getNetwork('1337'));
-const connector = new MetaMaskConnector({chains: [chain.hardhat]});
+const metamaskIsInstalled = window.ethereum && window.ethereum.isMetaMask;
+
+// const ethProvider = new providers.JsonRpcProvider("https://8545-qq7-gsn-8jeb69254es.ws-eu49.gitpod.io/", providers.getNetwork("1337"));
+
+// const connector = new MetaMaskConnector({ chains: [chain.hardhat, chain.mainnet] });
+
+const { chains, provider: ethProvider } = configureChains([...defaultChains, chain.hardhat], [
+  publicProvider()
+])
 
 const client = createClient({
   autoConnect: true,
-  provider: ethProvider,
-  connectors: [connector]
-})
+  connectors: [new InjectedConnector()],
+  provider: (config) => {
+    if (metamaskIsInstalled) {
+      return new providers.Web3Provider(window.ethereum as providers.ExternalProvider)
+    }
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+    return getDefaultProvider(config.chainId)
+  }
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 
 root.render(
   <Provider store={store}>
     <BrowserRouter>
       <ErrorBoundary>
-      <WagmiConfig client={client}>
-        <Routes>
-          <Route path="/" element={<App />} />
-        </Routes>
-      </WagmiConfig>
+        <WagmiConfig client={client}>
+          <Routes>
+            <Route path="/" element={<App />} />
+          </Routes>
+          <ToastContainer
+            toastClassName="test"
+            position="top-right"
+            autoClose={false}
+            newestOnTop
+            style={{ width: "45vw" }}
+            closeOnClick={false}
+            rtl={false}
+            pauseOnFocusLoss
+            draggable={false}
+          />
+        </WagmiConfig>
       </ErrorBoundary>
     </BrowserRouter>
-  </Provider>
-);
-
+  </Provider>);
--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,28 +1,32 @@
 // import {RootState} from "../store";
 import React from "react";
-
-import MetamaskButton from "../feature/MetamaskButton";
-import Relay from "../feature/Relay/Relay";
 import { useAccount } from "wagmi";
 
+import MetamaskButton from "../components/MetamaskButton";
+import DisconnectButton from "../components/DisconnectButton";
+import Relay from "../feature/Relay/Relay";
+
 import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button"
+
 
 export default function App() {
   // const { data, error, isLoading, isIdle, isFetching, isRefetching, isError, isSuccess } = useAccount()
-  const { data, error, isError } = useAccount();
+  const { data: account, error, isError } = useAccount();
 
   return (
     <div className="App">
-
       <Container className="my-1">
         {isError ?
           <span>{error?.message}</span> : <span>{isError}</span>
         }
         {
-          data !== undefined ?
-            <Relay />
-            :
-            <MetamaskButton />
+          account ?
+            <>
+              <Relay />
+              <hr />
+              <DisconnectButton />
+            </> : <MetamaskButton />
         }
       </Container>
     </div>
