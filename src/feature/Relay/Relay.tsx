5ef1c71e0cd1143c26ae2d25a419ad9f1f9c8c64
qq7 <qq7dev@gmail.com>
2022-06-27 11:20:47 +0300

    refresh on chain switch (due to HubAuthorize breaking)

--- src/feature/Relay/Relay.tsx
+++ src/feature/Relay/Relay.tsx
@@ -15,8 +15,9 @@ import LoadingButton from '../../components/LoadingButton'
 
 function Relay() {
   const relay = useAppSelector((state) => state.relay)
-  const dispatch = useAppDispatch()
   const chainId = Number(relay.relay.chainId)
+
+  const dispatch = useAppDispatch()
 
   const {
     activeChain,
@@ -35,7 +36,6 @@ function Relay() {
   })
 
   const ChainIdHandler = () => {
-    console.log(chainId, activeChain, error)
     return (
       <Alert variant='warning'>
         <Alert.Heading>Wrong chain</Alert.Heading>
@@ -51,44 +51,50 @@ function Relay() {
     )
   }
 
-  return (
-    <div className="row">
-      {relay.errorMsg !== '' ? relay.errorMsg : null}
-      {relay.relay.version !== undefined
-        ? <>
-          {/* TODO: extract a RelayData component */}
-          <details>
-            <summary>Show relay data: <span>{relay.relay.ownerAddress}</span></summary>
-            {
-              Object.keys(relay.relay).map((x, i) => {
-                return <div key={i}>{x}: {(relay.relay[x as keyof PingResponse])?.toString()}</div>
-              })
-            }
-          </details>
-          {activeChain?.id !== chainId && switchNetwork !== undefined
-            ? <ChainIdHandler />
-            : <RelayCommands />
+  if (Object.keys(relay.relay).length === 0) {
+    return (
+      <Form onSubmit={getRelayForm.handleSubmit}>
+        <Form.Label htmlFor="url">Relay URL
+          <Form.Control
+            id="url"
+            name="url"
+            type="url"
+            onChange={getRelayForm.handleChange}
+            value={getRelayForm.values.url}
+          />
+        </Form.Label>
+        <br />
+        <Button variant="success" type="submit">Fetch data</Button>
+      </Form>
+    )
+  }
+  if (relay.errorMsg !== '') return <span>{relay.errorMsg}</span>
+  if ((activeChain?.id !== chainId && !isLoading) || isLoading) {
+    return <ChainIdHandler />
+  }
+
+  if (isLoading) return <>Loading?</>
+  if (activeChain?.id === chainId && Object.keys(relay.relay).length > 0 && !isLoading) {
+    return (
+      <div className='row'>
+        {/* TODO: extract a RelayData component */}
+        <details>
+          <summary>Show relay data: <span>{relay.relay.ownerAddress}</span></summary>
+          {
+            Object.keys(relay.relay).map((x, i) => {
+              return <div key={i}>{x}: {(relay.relay[x as keyof PingResponse])?.toString()}</div>
+            })
           }
-          <Button variant="secondary"
-            className="my-2"
-            onClick={() => dispatch(deleteRelayData())}
-          >Switch</Button>
-        </>
-        : <Form onSubmit={getRelayForm.handleSubmit}>
-          <Form.Label htmlFor="url">Relay URL
-            <Form.Control
-              id="url"
-              name="url"
-              type="url"
-              onChange={getRelayForm.handleChange}
-              value={getRelayForm.values.url}
-            />
-          </Form.Label>
-          <br />
-          <Button variant="success" type="submit">Fetch data</Button>
-        </Form>
-      }
-    </div>
-  )
+        </details>
+        <RelayCommands />
+        <Button variant="secondary"
+          className="my-2"
+          onClick={() => dispatch(deleteRelayData())}
+        >Switch</Button>
+      </div>
+    )
+  }
+
+  return <>Error initializing relay view</>
 }
 export default React.memo(Relay)
--- src/index.tsx
+++ src/index.tsx
@@ -1,49 +1,44 @@
-import ReactDOM from "react-dom/client";
-import { WagmiConfig, configureChains, defaultChains, createClient, chain } from "wagmi";
-// import { MetaMaskConnector } from "wagmi/connectors/metaMask";
-import { MetaMaskInpageProvider } from "@metamask/providers";
-import { InjectedConnector } from "wagmi/connectors/injected";
-import { providers, getDefaultProvider, ethers } from "ethers";
-import { publicProvider } from 'wagmi/providers/public'
+import ReactDOM from 'react-dom/client'
+import { WagmiConfig, createClient } from 'wagmi'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { providers, getDefaultProvider } from 'ethers'
 import {
   BrowserRouter,
   Routes,
-  Route,
-} from "react-router-dom";
-import { ToastContainer } from "react-toastify";
+  Route
+} from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 
-import App from "./routes/App";
-import store from "./store";
-import { Provider } from "react-redux";
+import App from './routes/App'
+import store from './store'
+import { Provider } from 'react-redux'
 
-import { ErrorBoundary } from "./components/ErrorBoundary";
+import { ErrorBoundary } from './components/ErrorBoundary'
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "react-toastify/dist/ReactToastify.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'react-toastify/dist/ReactToastify.css'
 
-const metamaskIsInstalled = window.ethereum && window.ethereum.isMetaMask;
-
-// const ethProvider = new providers.JsonRpcProvider("https://8545-qq7-gsn-8jeb69254es.ws-eu49.gitpod.io/", providers.getNetwork("1337"));
-
-// const connector = new MetaMaskConnector({ chains: [chain.hardhat, chain.mainnet] });
-
-const { chains, provider: ethProvider } = configureChains([...defaultChains, chain.hardhat], [
-  publicProvider()
-])
+const metamaskIsInstalled = window.ethereum?.isMetaMask
 
 const client = createClient({
   autoConnect: true,
   connectors: [new InjectedConnector()],
   provider: (config) => {
-    if (metamaskIsInstalled) {
-      return new providers.Web3Provider(window.ethereum as providers.ExternalProvider)
+    if (metamaskIsInstalled === true) {
+      const provider = new providers.Web3Provider(window.ethereum as providers.ExternalProvider, 'any')
+      provider.on('network', (newNetwork, oldNetwork) => {
+        if (oldNetwork !== null) {
+          window.location.reload()
+        }
+      })
+      return provider
     }
 
     return getDefaultProvider(config.chainId)
   }
-});
+})
 
-const root = ReactDOM.createRoot(document.getElementById("root")!);
+const root = ReactDOM.createRoot(document.getElementById('root') as Element)
 
 root.render(
   <Provider store={store}>
@@ -54,11 +49,10 @@ root.render(
             <Route path="/" element={<App />} />
           </Routes>
           <ToastContainer
-            toastClassName="test"
             position="top-right"
             autoClose={false}
             newestOnTop
-            style={{ width: "45vw" }}
+            style={{ width: '45vw' }}
             closeOnClick={false}
             rtl={false}
             pauseOnFocusLoss
@@ -67,4 +61,4 @@ root.render(
         </WagmiConfig>
       </ErrorBoundary>
     </BrowserRouter>
-  </Provider>);
+  </Provider>)
