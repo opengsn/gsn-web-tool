b492e38965aee6c3552b2187ed600a8b07349431
qq7 <qq7dev@gmail.com>
2022-07-08 12:59:12 +0300

    OG-834 show if current acc is owner in StakeManager

--- src/feature/RelayInfo/StakeInfo.tsx
+++ src/feature/RelayInfo/StakeInfo.tsx
@@ -1,5 +1,6 @@
-import { useContractRead } from 'wagmi'
+import { useAccount, useContractRead } from 'wagmi'
 
+import { isSameAddress } from '@opengsn/common/dist/Utils'
 import { Address } from '@opengsn/common/dist/types/Aliases'
 import StakeManagerAbi from '../../contracts/stakeManager.json'
 
@@ -11,6 +12,7 @@ interface stakeInfoProps {
 }
 
 export default function StakeInfo ({ stakeManagerAddress, relayManagerAddress }: stakeInfoProps) {
+  const { address } = useAccount()
   const { data: stakeInfo } = useContractRead({
     addressOrName: stakeManagerAddress,
     contractInterface: StakeManagerAbi,
@@ -21,13 +23,21 @@ export default function StakeInfo ({ stakeManagerAddress, relayManagerAddress }:
 
   if (stakeInfo !== undefined) {
     const { owner, token } = stakeInfo[0]
+    const ShowOwner = () => {
+      if (address !== undefined && isSameAddress(address, owner)) {
+        return <span>(our account)</span>
+      } else {
+        return <span>(not our account)</span>
+      }
+    }
 
     return (
-      <div>
-        <div>current owner: {owner}</div>
+      <>
+        <div>current owner: {owner} <ShowOwner /></div>
         <StakingTokenInfo stakingToken={token} />
-      </div>
+      </>
     )
   }
+
   return <div>Failed to fetch stake data</div>
 }
