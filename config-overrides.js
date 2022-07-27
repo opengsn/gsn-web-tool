167bd40bfee569129f68286559fc629754b57eea
qq7 <qq7dev@gmail.com>
2022-07-27 17:55:34 +0300

    lint --fix

--- config-overrides.js
+++ config-overrides.js
@@ -1,18 +1,18 @@
-const { resolve } = require('path');
-const webpack = require('webpack');
-module.exports = function override(config) {
-  const fallback = config.resolve.fallback || {};
+const { resolve } = require('path')
+const webpack = require('webpack')
+module.exports = function override (config) {
+  const fallback = config.resolve.fallback || {}
   Object.assign(fallback, {
-    "crypto": require.resolve("crypto-browserify"),
-    "fs": false,
-    "stream": require.resolve("stream-browserify"),
-    "assert": require.resolve("assert"),
-    "http": require.resolve("stream-http"),
-    "https": require.resolve("https-browserify"),
-    "os": require.resolve("os-browserify"),
-    "url": require.resolve("url")
+    crypto: require.resolve('crypto-browserify'),
+    fs: false,
+    stream: require.resolve('stream-browserify'),
+    assert: require.resolve('assert'),
+    http: require.resolve('stream-http'),
+    https: require.resolve('https-browserify'),
+    os: require.resolve('os-browserify'),
+    url: require.resolve('url')
   })
-  config.resolve.fallback = fallback;
+  config.resolve.fallback = fallback
   config.resolve.extensions = ['.ts', '.js', '.tsx', '.jsx']
   config.plugins = (config.plugins || []).concat([
     new webpack.ProvidePlugin({
@@ -20,5 +20,5 @@ module.exports = function override(config) {
       Buffer: ['buffer', 'Buffer']
     })
   ])
-  return config;
+  return config
 }
