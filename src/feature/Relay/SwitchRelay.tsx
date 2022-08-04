5e570d7e0d039ed6ea8e9d96353e9cfdf0a446c4
qq7 <qq7dev@gmail.com>
2022-08-04 15:01:46 +0000

    OG-871 improve relay input formatting (#14)
    * OG-871 improve relay input formatting
    
    * deleteRelay: thunk -> reducer
    
    * fix code; delete commented out code
    
    * Update DisconnectButton.tsx

--- src/feature/Relay/SwitchRelay.tsx
+++ src/feature/Relay/SwitchRelay.tsx
@@ -1,14 +1,23 @@
 import Button from 'react-bootstrap/Button'
+import { useSearchParams } from 'react-router-dom'
 import { useAppDispatch } from '../../hooks'
 import { deleteRelayData } from './relaySlice'
 
-export default function SwitchRelayButton () {
+export default function SwitchRelayButton ({ autoFocus, abortFetch }: { autoFocus?: boolean, abortFetch?: unknown }) {
   const dispatch = useAppDispatch()
+  const [searchParams, setSearchParams] = useSearchParams()
   const handleDeleteRelayData = () => {
-    dispatch(deleteRelayData()).catch(console.error)
+    searchParams.delete('relayUrl')
+    setSearchParams(searchParams)
+    if (abortFetch !== undefined) {
+      const abortFetchFunc = abortFetch as Function
+      abortFetchFunc()
+    }
+    dispatch(deleteRelayData())
   }
   return (<Button variant="secondary"
     className="my-2"
     onClick={handleDeleteRelayData}
+    autoFocus={autoFocus !== undefined ? autoFocus : false}
   >Switch relay</Button>)
 }
