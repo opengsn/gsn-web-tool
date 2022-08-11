f3ac32850b0fe32d59a35e9989e6c0eba33abdcc
qq7 <qq7dev@gmail.com>
2022-08-11 22:06:12 +0300

    simplify 'advanced' minting menu

--- src/feature/RelayCommands/RegisterRelay/StakeWithERC20/Minter/MintAmountForm.tsx
+++ src/feature/RelayCommands/RegisterRelay/StakeWithERC20/Minter/MintAmountForm.tsx
@@ -1,5 +1,4 @@
-import { useRef, useEffect, useContext } from 'react'
-import { useFormik } from 'formik'
+import { useEffect, useContext, useRef, useState } from 'react'
 import { ethers } from 'ethers'
 
 import Form from 'react-bootstrap/Form'
@@ -9,23 +8,29 @@ import { MinterContext } from './Minter'
 import { TokenContext } from '../StakeWithERC20'
 
 export default function MintAmountForm () {
+  const [localMintAmount, setLocalMintAmount] = useState(ethers.constants.Zero)
   const { minimumStakeForToken } = useContext(TokenContext)
-  const { outstandingMintAmount, mintAmount, setMintAmount } = useContext(MinterContext)
+  const { setMintAmount } = useContext(MinterContext)
+  const inputValue = useRef<HTMLInputElement>(null)
 
-  let initialAmount = Number(ethers.utils.formatEther(mintAmount))
-  if (outstandingMintAmount !== null) initialAmount = Number(ethers.utils.formatEther(mintAmount))
+  const handleSetMintAmount = () => {
+    try {
+      const value = inputValue?.current?.value
+      if (value === undefined) return
 
-  const getMintAmountForm = useFormik({
-    initialValues: {
-      amount: initialAmount
-    },
-    onSubmit: values => {
-      console.log(outstandingMintAmount)
-      console.log(typeof values.amount, values.amount, mintAmount)
-      const amountBigNumber = ethers.utils.parseEther(values.amount.toString())
-      setMintAmount(amountBigNumber)
+      const amountBigNumber = ethers.utils.parseEther(value)
+      setLocalMintAmount(amountBigNumber)
+    } catch (e: any) {
+      console.log(e)
+      // suppress error
     }
-  })
+  }
+
+  useEffect(() => {
+    if (localMintAmount !== ethers.constants.Zero) {
+      setMintAmount(localMintAmount)
+    }
+  }, [localMintAmount])
 
   useEffect(() => {
     return () => {
@@ -33,37 +38,18 @@ export default function MintAmountForm () {
     }
   }, [minimumStakeForToken, setMintAmount])
 
-  let valueIsValidAmount = false
-  try {
-    ethers.utils.parseEther(getMintAmountForm.values.amount.toString())
-    valueIsValidAmount = true
-  } catch (e: any) {
-    console.log(e)
-    // suppress error
-  }
-
   return (
-    <Form onSubmit={getMintAmountForm.handleSubmit} className="my-2">
+    <Form className="my-2">
       <Form.Label>
         <Form.Control
           id="amount"
           name="amount"
-          type="number"
-          lang="en"
-          min="0"
-          step="any"
-          onChange={getMintAmountForm.handleChange}
-          value={getMintAmountForm.values.amount}
+          type="text"
+          ref={inputValue}
+          onChange={() => handleSetMintAmount()}
         />
       </Form.Label>
       <br />
-      <ButtonGroup>
-        <Button disabled={!valueIsValidAmount} variant="success" type="submit">Change mint amount</Button>
-        <Button onClick={() => {
-          getMintAmountForm.resetForm()
-          setMintAmount(minimumStakeForToken)
-        }}>Reset</Button>
-      </ButtonGroup>
     </Form>
   )
 }
--- src/feature/RelayCommands/RegisterRelay/StakeWithERC20/Minter/MintButton.tsx
+++ src/feature/RelayCommands/RegisterRelay/StakeWithERC20/Minter/MintButton.tsx
@@ -11,44 +11,46 @@ import ErrorButton from '../../../../../components/ErrorButton'
 import { TokenContext } from '../StakeWithERC20'
 import { MinterContext } from './Minter'
 import TransactionSuccessToast from '../../../../../components/TransactionSuccessToast'
+import LoadingButton from '../../../../../components/LoadingButton'
 
 export default function MintButton () {
   const { token } = useContext(TokenContext)
   const { mintAmount } = useContext(MinterContext)
   const defaultStateSwitchers = useDefaultStateSwitchers()
 
-  const { error: mintTokenError, isError, isLoading, write: mintToken } = useContractWrite(
-    {
-      addressOrName: token,
-      contractInterface: iErc20TokenAbi,
-      functionName: 'deposit',
-      overrides: { value: mintAmount },
-      onSuccess (data) {
-        const text = 'Minting token'
-        toast.info(<TransactionSuccessToast text={text} hash={data.hash} />)
-      },
-      ...defaultStateSwitchers
+  const { error: mintTokenError, isError, isLoading, isSuccess, write: mintToken } = useContractWrite({
+    addressOrName: token,
+    contractInterface: iErc20TokenAbi,
+    functionName: 'deposit',
+    overrides: { value: mintAmount },
+    mode: 'recklesslyUnprepared',
+    ...defaultStateSwitchers,
+    onSuccess (data) {
+      const text = 'Minting token'
+      toast.info(<TransactionSuccessToast text={text} hash={data.hash} />)
     }
-  )
+  })
 
   const MintButton = () => {
-    const MintSuccess = <div>Minted succesfully</div>
-
+    const MintSuccess = () => { return <div>Minted succesfully</div> }
     const MintError = () => {
       const text = `Mint outstanding amount: ${ethers.utils.formatEther(mintAmount)}`
       return (
-        <ErrorButton message={mintTokenError?.message} onClick={() => mintToken()}>
+        <ErrorButton message={mintTokenError?.message} onClick={() => mintToken?.()}>
           <span>Retry {text}</span>
         </ErrorButton>
       )
     }
+
+    if (isLoading) return <LoadingButton />
+    if (isSuccess) return <MintSuccess />
 
     const text = <span>Mint {ethers.utils.formatEther(mintAmount)}</span>
     return (
       <>
         {isError
           ? <MintError />
-          : <Button onClick={() => mintToken()} className="mt-2">{text}</Button>
+          : <Button onClick={() => mintToken?.()} className="mt-2">{text}</Button>
         }
       </>
     )
