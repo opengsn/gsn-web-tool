2329eec935eff0272766fab02f0202bb94f13d25
qq7 <qq7dev@gmail.com>
2022-09-07 16:23:56 +0300

    remove commented out

--- src/index.tsx
+++ src/index.tsx
@@ -19,23 +19,8 @@ import { ErrorBoundary } from './components/ErrorBoundary'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'react-toastify/dist/ReactToastify.css'
-import RelaysListWagmiWrapper from './feature/RelaysList/RelaysListWagmiWrapper'
-
-// const client = createClient({
-//   autoConnect: true,
-//   connectors: [new InjectedConnector()],
-//   provider: (config) => {
-//     if (config.chainId !== undefined) {
-//       const provider = new providers.Web3Provider(window.ethereum as providers.ExternalProvider, config.chainId)
-//       return provider
-//     }
-
-//     return getDefaultProvider()
-//   }
-// })
 
 const root = ReactDOM.createRoot(document.getElementById('root') as Element)
-// const wrappedApp = <WagmiConfig client={client}><App /></WagmiConfig>
 
 root.render(
   <Provider store={store}>
