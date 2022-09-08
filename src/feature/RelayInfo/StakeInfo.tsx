87b8b2cc416502700a4a17d3016afcb0b0d85df1
qq7 <qq7dev@gmail.com>
2022-09-08 15:33:23 +0300

    add comma

--- src/feature/RelayInfo/StakeInfo.tsx
+++ src/feature/RelayInfo/StakeInfo.tsx
@@ -24,7 +24,7 @@ export default function StakeInfo ({ stakeManagerAddress, relayManagerAddress }:
     contractInterface: StakeManagerAbi,
     functionName: 'getStakeInfo',
     args: relayManagerAddress,
-    chainId
+    chainId,
     watch: false,
     onSuccess (data) {
       dispatch(validateConfigOwnerInLineWithStakeManager(data[0].owner))
