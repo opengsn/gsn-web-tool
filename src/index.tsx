9c30c1c0cc29a89043bd717be1a10236bf79eda4
qq7 <qq7dev@gmail.com>
2022-09-17 20:34:15 +0300

    Revert to using BrowserRouter instead of HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 import App from './routes/App'
@@ -14,9 +14,9 @@ const root = ReactDOM.createRoot(document.getElementById('root') as Element)
 
 root.render(
   <Provider store={store}>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <Router basename={process.env.PUBLIC_URL}>
       <ErrorBoundary>
         <App />
       </ErrorBoundary>
-    </HashRouter>
+    </Router>
   </Provider >)
