4b9ddcdf5f5840187fdb9d4bdf0edaccd90cbc48
qq7 <qq7dev@gmail.com>
2022-09-27 13:46:48 +0000

    allow using IP as a relay URL in input

--- src/feature/Relay/RelayUrlForm.tsx
+++ src/feature/Relay/RelayUrlForm.tsx
@@ -10,6 +10,8 @@ import InputGroup from 'react-bootstrap/InputGroup'
 
 import { Flip, toast } from 'react-toastify'
 
+import { isIP } from 'is-ip'
+
 import { ROUTES } from '../../constants/routes'
 import { useAppDispatch, useAppSelector } from '../../hooks'
 import { PingResponse } from '../../types'
@@ -50,14 +52,18 @@ export default function RelayUrlForm () {
       const withGetaddr = (url: string) => !/\/getaddr/i.test(url) ? `${url}/getaddr` : url
 
       const formatURL = (url: string) => withHttps(withGetaddr(removeTrailingSlashes(url)))
+      const formatLocalhost = (url: string) => withGetaddr(removeTrailingSlashes(url))
 
       const extractedURL = values.url.match(regexpURL)
 
       let URL: string
-      if (extractedURL !== null) {
+      if (values.url.includes('localhost')) {
+        URL = formatLocalhost(values.url)
+      } else if (extractedURL !== null) {
         URL = formatURL(extractedURL[0])
-      } else if (values.url.includes('localhost')) {
-        URL = values.url
+        if (isIP(extractedURL[0].split(':')[0])) {
+          URL = formatLocalhost(values.url)
+        }
       } else {
         toast.dismiss()
         toast.error('Please enter a valid URL', { position: 'top-center', hideProgressBar: true, autoClose: 1300, closeOnClick: true, transition: Flip })
@@ -69,7 +75,13 @@ export default function RelayUrlForm () {
           const search = createSearchParams({ relayUrl: URL }).toString()
           navigate({ pathname: ROUTES.DetailedView, search })
         } else if (res.type.includes('rejected')) {
-          if (URL.includes('localhost')) toast.info('endpoint must return relay config as JSON', { autoClose: 2000, closeButton: true })
+          const urlFormatMessage = 'endpoint must return relay config as JSON and include HTTP or HTTPS'
+          if (URL.includes('localhost')) {
+            toast.info(
+              urlFormatMessage,
+              { autoClose: 2000, closeButton: true }
+            )
+          }
         }
       }).catch((err) => {
         toast.error('error while fetching relay data. try refreshing the page')
@@ -108,7 +120,9 @@ export default function RelayUrlForm () {
   }
 
   if (relay.errorMsg !== '') {
-    return <div>smt</div>
+    return <>
+      <Alert variant='danger'><span>{relay.errorMsg}</span></Alert>
+    </>
   }
 
   return <div>Relay data is already fetched. Refresh the page.</div>
