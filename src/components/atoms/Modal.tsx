52ab36cac229d420fa61ae34b3905fe6b62fa3c9
Hadar Hubara <76662798+hadarhubara10@users.noreply.github.com>
2023-04-30 19:55:08 +0300

    changed modal to dialog (#72)

--- src/components/atoms/Modal.tsx
+++ src/components/atoms/Modal.tsx
@@ -1,6 +1,5 @@
-import { Modal as MuiModal } from '@mui/material'
+import { DialogContent, Dialog as MuiDialog } from '@mui/material'
 import { FC, ReactNode } from 'react'
-import { Box } from '../atoms'
 
 interface IProps {
   children: ReactNode
@@ -8,29 +7,17 @@ interface IProps {
   onClose?: () => void
 }
 
-const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  width: '50%',
-  height: '50%',
-  overflow: 'auto',
-  transform: 'translate(-50%, -50%)',
-  bgcolor: 'background.paper',
-  textAlign: 'center',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  border: 0,
-  boxShadow: 24,
-  p: 4
-}
-
 const Modal: FC<IProps> = ({ children, open, onClose }) => {
   return (
-    <MuiModal open={open} onClose={onClose}>
-      <Box sx={style}>{children}</Box>
-    </MuiModal>
+    <MuiDialog
+      open={open}
+      onClose={onClose}
+      sx={{
+        textAlign: 'center'
+      }}
+    >
+      <DialogContent>{children}</DialogContent>
+    </MuiDialog>
   )
 }
 
