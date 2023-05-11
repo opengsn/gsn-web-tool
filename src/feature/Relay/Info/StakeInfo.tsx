3bce7fc29bf018aa6be6f53183d73fd6f8fa085c
hadarhubara10 <hadarhubara10@gmail.com>
2023-05-11 10:18:28 +0300

    stake info

--- src/feature/Relay/Info/StakeInfo.tsx
+++ src/feature/Relay/Info/StakeInfo.tsx
@@ -1,10 +1,6 @@
-import { useAccount, useContractRead } from 'wagmi'
-
+import { useContractRead } from 'wagmi'
 import { useAppDispatch, useAppSelector } from '../../../hooks'
-import { isSameAddress } from '../../../utils'
-
 import { validateConfigOwnerInLineWithStakeManager } from '../relaySlice'
-
 import StakeManager from '../../../contracts/StakeManager.json'
 import StakingToken from './StakeManagerInfo/StakingToken'
 import { TableCell, TableRow, Typography } from '../../../components/atoms'
@@ -19,38 +15,27 @@ export default function StakeInfo({ stakeManagerAddress, relayManagerAddress }: 
   const chainId = Number(relayData.chainId)
 
   const dispatch = useAppDispatch()
-  const { address } = useAccount()
   const { data: stakeInfo, isSuccess } = useContractRead({
     address: stakeManagerAddress as any,
     abi: StakeManager.abi,
     functionName: 'getStakeInfo',
     args: [relayManagerAddress],
+    enabled: relayManagerAddress !== '',
     chainId,
     onSuccess(data) {
       dispatch(validateConfigOwnerInLineWithStakeManager((data as any)[0].owner))
-    },
-    onError(err) {
-      console.log('error fetching data from stake manager.', { cause: err })
     }
   })
 
   if (stakeInfo !== undefined && isSuccess) {
-    const { owner, token } = (stakeInfo as any)[0]
-    const ShowOwner = () => {
-      if (address !== undefined && isSameAddress(address, owner)) {
-        return <b>currently connected</b>
-      } else {
-        return <b>not the connected account</b>
-      }
-    }
-
+    const { token } = (stakeInfo as any)[0]
     return <StakingToken stakingToken={token} chainId={chainId} />
   }
 
   const LoadingRow = (
     <TableRow>
       <TableCell>
-        <Typography variant={'subtitle2'}>Loading info from stakemanager... If , refreshing the page might help</Typography>
+        <Typography variant={'subtitle2'}>Loading info from stake manager, refreshing the page might help</Typography>
       </TableCell>
     </TableRow>
   )
