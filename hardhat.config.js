c39897cfa47f9ec4de619c0ae6f6c2964a844e0c
Alex Forshtat <forshtat1@gmail.com>
2023-07-10 01:27:21 +0300

    Add scripts to run hardhat with chainId '1'; fix some errors in logs

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,10 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const chainId = process.env.CHAINID == null ? 1337 : parseInt(process.env.CHAINID)
+
 module.exports = {
-  solidity: '0.8.9'
+  solidity: '0.8.9',
+  networks: {
+    hardhat: { chainId }
+  }
 }
